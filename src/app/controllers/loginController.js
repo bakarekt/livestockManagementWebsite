30ac9c6210a11c2604b6c36c9a985ca2a2c56278
bakarekt <nvdung1020@gmail.com>
2024-12-04 20:29:55 +0700

    Thêm giao diện login

---
+++ src/app/controllers/loginController.js
@@ -0,0 +1,9 @@
+class LoginController {
+    //GET edit
+    login(req, res) {
+      res.render("login");
+    }
+  }
+  
+  module.exports = new LoginController();
+  
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,12 @@ const searchRouter = require("./searchRoutes");
 const editRouter = require("./editRoutes");
 const siteRouter = require("./site");
 const userRouter = require("./userRoutes");
+const loginRouter = require("./loginRoutes");
 
 function route(app) {
+
+  app.use("/login", loginRouter);
+
   app.use("/user", userRouter);
 
   app.use("/edit", editRouter);
@@ -15,9 +19,9 @@ function route(app) {
   app.get("/login", (req, res) => {
     res.render("login");
   });
-  app.post("/login", (req, res) => {
-    res.send("login");
-  });
+  // app.post("/login", (req, res) => {
+  //   res.send("login");
+  // }); ------- Đang nghiên cứu
 }
 
 module.exports = route;
